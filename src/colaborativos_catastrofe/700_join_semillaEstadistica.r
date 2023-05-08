fe9b9e4283d6ca00e92aca96e725fe4c39a9d833
MarceDistefano <marcelitadis@gmail.com>
2023-05-08 18:29:11 -0300

    "pred estadística"

---
+++ src/colaborativos_catastrofe/700_join_semillaEstadistica.r
@@ -0,0 +1,98 @@
+# Experimentos Colaborativos Default
+#limpio la memoria
+
+rm( list= ls(all.names= TRUE) )  #remove all objects
+gc( full= TRUE )                 #garbage collection
+
+require("data.table")
+
+
+dataset_grande <- fread ("~/buckets/b1/.datasets/competencia_2023.csv.gz")
+
+#semilla1
+
+dataset_pred1 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla1/pred_01_006.csv")
+
+dataset_pred[dataset_grande,
+             on= c("numero_de_cliente","foto_mes"),
+             clase_ternaria:=i.clase_ternaria]
+
+dataset_pred1 [, ganancia:= -3000]
+dataset_pred1 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred1, - prob)
+
+dataset_pred1 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred1 [, envios:= .I]
+
+#semilla 2
+
+dataset_pred2 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla2/pred_01_006.csv")
+
+dataset_pred2[dataset_grande,
+             on= c("numero_de_cliente","foto_mes"),
+             clase_ternaria:=i.clase_ternaria]
+
+
+dataset_pred2 [, ganancia:= -3000]
+dataset_pred2 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred2, - prob)
+
+dataset_pred2 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred2 [, envios:= .I]
+
+#semilla 3
+
+dataset_pred3 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla3/pred_01_006.csv")
+
+dataset_pred3[dataset_grande,
+             on= c("numero_de_cliente","foto_mes"),
+             clase_ternaria:=i.clase_ternaria]
+
+dataset_pred3 [, ganancia:= -3000]
+dataset_pred3 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred3, - prob)
+
+dataset_pred3 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred3 [, envios:= .I]
+
+#semilla 4
+
+dataset_pred4 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla4/pred_01_006.csv")
+
+dataset_pred4[dataset_grande,
+             on= c("numero_de_cliente","foto_mes"),
+             clase_ternaria:=i.clase_ternaria]
+
+dataset_pred4 [, ganancia:= -3000]
+dataset_pred4 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred4, - prob)
+
+dataset_pred4 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred4 [, envios:= .I]
+
+#semilla 5
+
+dataset_pred5 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla5/pred_01_006.csv")
+
+dataset_pred5[dataset_grande,
+             on= c("numero_de_cliente","foto_mes"),
+             clase_ternaria:=i.clase_ternaria]
+
+dataset_pred5 [, ganancia:= -3000]
+dataset_pred5 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred5, - prob)
+
+dataset_pred5 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred5 [, envios:= .I]
+
+
