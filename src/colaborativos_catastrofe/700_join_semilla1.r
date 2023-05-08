a9e8796a4c05958f169e19e7d811905c6f5a19ea
MarceDistefano <marcelitadis@gmail.com>
2023-05-08 19:55:02 -0300

    corregidos

--- src/colaborativos_catastrofe/700_join_semilla1.r
+++ src/colaborativos_catastrofe/700_join_semilla1.r
@@ -7,11 +7,11 @@ gc( full= TRUE )                 #garbage collection
 require("data.table")
 
 
-dataset_grande <- fread ("~/buckets/b1/.datasets/competencia_2023.csv.gz")
+dataset_grande <- fread ("~/buckets/b1/datasets/competencia_2023.csv.gz")
 
 #semilla1
 
-dataset_pred1 <- fread ("~/buckets/b1/.exp/ZZ6910Ninguno_semilla1/pred_01_006.csv")
+dataset_pred1 <- fread ("~/buckets/b1/exp/ZZ6910Ninguno_semilla1/pred_01_006.csv")
 
 dataset_pred[dataset_grande,
              on= c("numero_de_cliente","foto_mes"),
@@ -28,7 +28,7 @@ dataset_pred1 [, envios:= .I]
 
 #semilla 2
 
-dataset_pred2 <- fread ("~/buckets/b1/.exp/ZZ6910Ninguno_semilla2/pred_01_006.csv")
+dataset_pred2 <- fread ("~/buckets/b1/exp/ZZ6910Ninguno_semilla2/pred_01_006.csv")
 
 dataset_pred2 [, ganancia:= -3000]
 dataset_pred2 [clase_ternaria== "BAJA+2", ganancia:= 117000]
@@ -41,7 +41,7 @@ dataset_pred2 [, envios:= .I]
 
 #semilla 3
 
-dataset_pred3 <- fread ("~/buckets/b1/.exp/ZZ6910Ninguno_semilla3/pred_01_006.csv")
+dataset_pred3 <- fread ("~/buckets/b1/exp/ZZ6910Ninguno_semilla3/pred_01_006.csv")
 
 dataset_pred3 [, ganancia:= -3000]
 dataset_pred3 [clase_ternaria== "BAJA+2", ganancia:= 117000]
@@ -54,7 +54,7 @@ dataset_pred3 [, envios:= .I]
 
 #semilla 4
 
-dataset_pred2 <- fread ("~/buckets/b1/.exp/ZZ6910Ninguno_semilla4/pred_01_006.csv")
+dataset_pred4 <- fread ("~/buckets/b1/exp/ZZ6910Ninguno_semilla4/pred_01_006.csv")
 
 dataset_pred4 [, ganancia:= -3000]
 dataset_pred4 [clase_ternaria== "BAJA+2", ganancia:= 117000]
@@ -67,4 +67,48 @@ dataset_pred4 [, envios:= .I]
 
 #semilla 5
 
+dataset_pred5 <- fread ("~/buckets/b1/exp/ZZ6910Ninguno_semilla4/pred_01_006.csv")
+
+dataset_pred5 [, ganancia:= -3000]
+dataset_pred5 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred5, - prob)
+
+dataset_pred5 [, ganancia_acumulada: = cumsum(ganancia)]
+
+dataset_pred5 [, envios:= .I]
+
+# Calcular el promedio de ganancia acumulada por envío
+
+promedio_ganancia <- rbindlist(list(dataset_pred1, dataset_pred2, dataset_pred3, dataset_pred4, dataset_pred5))[, .(ganancia_media = mean(ganancia_acumulada)), by = envios]
+
+#Gráfico
+
+library(ggplot2)
+
+
+# Subconjunto de datos para el gráfico (hasta 20,000 envíos)
+
+data_plot <- promedio_ganancia[promedio_ganancia$envios <= 20000, ]
+
+# Gráfico de ganancias acumuladas en función del número de envíos
+
+#ggplot(data_plot, aes(x = envios, y = ganancia_media)) +
+  #geom_line(size = 1, alpha = 0.7) +
+ # xlab("Número de envíos") +
+  #ylab("Ganancia ") +
+  #ggtitle("Promedio de Ganancias (por semillas)  en función del número de envíos")
+
+ggplot(data_plot, aes(x = envios, y = ganancia_media)) +
+  geom_line(size = 1, alpha = 0.7) +
+  scale_color_viridis_d() +
+  scale_y_continuous(labels = scales::unit_format(unit = "M", scale = 1e-6)) +
+  xlab("Número de envíos") +
+  ylab("Ganancia (millones)") +
+  ggtitle("Promedio de Ganancias en función del número de envíos") +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, color = "blue"),
+        axis.title = element_text(face = "bold", size = 14),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 12, color = "gray"))
 
--- src/colaborativos_catastrofe/700_join_semillaEstadistica.r
+++ src/colaborativos_catastrofe/700_join_semillaEstadistica.r
@@ -7,13 +7,13 @@ gc( full= TRUE )                 #garbage collection
 require("data.table")
 
 
-dataset_grande <- fread ("~/buckets/b1/.datasets/competencia_2023.csv.gz")
+dataset_grande <- fread ("~/buckets/b1/datasets/competencia_2023.csv.gz")
 
 #semilla1
 
-dataset_pred1 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla1/pred_01_006.csv")
+dataset_pred1 <- fread ("~/buckets/b1/exp/ZZ6912Estadistica_semilla1/pred_01_043.csv")
 
-dataset_pred[dataset_grande,
+dataset_pred1[dataset_grande,
              on= c("numero_de_cliente","foto_mes"),
              clase_ternaria:=i.clase_ternaria]
 
@@ -22,13 +22,13 @@ dataset_pred1 [clase_ternaria== "BAJA+2", ganancia:= 117000]
 
 setorder(dataset_pred1, - prob)
 
-dataset_pred1 [, ganancia_acumulada: = cumsum(ganancia)]
+dataset_pred1 [, ganancia_acumulada:= cumsum(ganancia)]
 
 dataset_pred1 [, envios:= .I]
 
 #semilla 2
 
-dataset_pred2 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla2/pred_01_006.csv")
+dataset_pred2 <- fread ("~/buckets/b1/exp/ZZ6912Estadistica_semilla2/pred_01_043.csv")
 
 dataset_pred2[dataset_grande,
              on= c("numero_de_cliente","foto_mes"),
@@ -40,13 +40,13 @@ dataset_pred2 [clase_ternaria== "BAJA+2", ganancia:= 117000]
 
 setorder(dataset_pred2, - prob)
 
-dataset_pred2 [, ganancia_acumulada: = cumsum(ganancia)]
+dataset_pred2 [, ganancia_acumulada:= cumsum(ganancia)]
 
 dataset_pred2 [, envios:= .I]
 
 #semilla 3
 
-dataset_pred3 <- fread ("~/buckets/b1/.exp/ZZ6912Estadistica_semilla3/pred_01_006.csv")
+dataset_pred3 <- fread ("~/buckets/b1/exp/ZZ6912Estadistica_semilla3/pred_01_043.csv")
 
 dataset_pred3[dataset_grande,
              on= c("numero_de_cliente","foto_mes"),
@@ -57,10 +57,77 @@ dataset_pred3 [clase_ternaria== "BAJA+2", ganancia:= 117000]
 
 setorder(dataset_pred3, - prob)
 
-dataset_pred3 [, ganancia_acumulada: = cumsum(ganancia)]
+dataset_pred3 [, ganancia_acumulada:= cumsum(ganancia)]
 
 dataset_pred3 [, envios:= .I]
 
+#semilla 4
+
+dataset_pred4<- fread ("~/buckets/b1/exp/ZZ6912Estadistica_semilla4/pred_01_043.csv")
+
+dataset_pred4[dataset_grande,
+              on= c("numero_de_cliente","foto_mes"),
+              clase_ternaria:=i.clase_ternaria]
+
+dataset_pred4 [, ganancia:= -3000]
+dataset_pred4 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred4, - prob)
+
+dataset_pred4 [, ganancia_acumulada:= cumsum(ganancia)]
+
+dataset_pred4 [, envios:= .I]
+
+#semilla 5
+
+dataset_pred5 <- fread ("~/buckets/b1/exp/ZZ6912Estadistica_semilla5/pred_01_043.csv")
+
+dataset_pred5[dataset_grande,
+              on= c("numero_de_cliente","foto_mes"),
+              clase_ternaria:=i.clase_ternaria]
+
+dataset_pred5 [, ganancia:= -3000]
+dataset_pred5 [clase_ternaria== "BAJA+2", ganancia:= 117000]
+
+setorder(dataset_pred5, - prob)
+
+dataset_pred5 [, ganancia_acumulada:= cumsum(ganancia)]
+
+dataset_pred5 [, envios:= .I]
+
+
 # Calcular el promedio de ganancia acumulada por envío
 
-promedio_ganancia <- rbindlist(list(dataset_pred1, dataset_pred2, dataset_pred3, dataset_pred4, dataset_pred5))[, .(promedio_ganancia = mean(ganancia_acumulada)), by = envios]
+promedio_ganancia <- rbindlist(list(dataset_pred1, dataset_pred2, dataset_pred3, dataset_pred4, dataset_pred5))[, .(ganancia_media = mean(ganancia_acumulada)), by = envios]
+
+#Gráfico
+
+library(ggplot2)
+
+
+# Subconjunto de datos para el gráfico (hasta 20,000 envíos)
+
+data_plot <- promedio_ganancia[promedio_ganancia$envios <= 20000, ]
+
+# Gráfico de ganancias acumuladas en función del número de envíos
+
+#ggplot(data_plot, aes(x = envios, y = ganancia_media)) +
+  #geom_line(size = 1, alpha = 0.7) +
+ # xlab("Número de envíos") +
+  #ylab("Ganancia ") +
+  #ggtitle("Promedio de Ganancias (por semillas)  en función del número de envíos")
+
+ggplot(data_plot, aes(x = envios, y = ganancia_media)) +
+  geom_line(size = 1, alpha = 0.7) +
+  scale_color_viridis_d() +
+  scale_y_continuous(labels = scales::unit_format(unit = "M", scale = 1e-6)) +
+  xlab("Número de envíos") +
+  ylab("Ganancia (millones)") +
+  ggtitle("Promedio de Ganancias en función del número de envíos") +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, color = "blue"),
+        axis.title = element_text(face = "bold", size = 14),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 12, color = "gray"))
+
+        
