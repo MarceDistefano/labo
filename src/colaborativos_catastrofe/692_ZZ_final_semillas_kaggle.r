01b51c86490823f2a9c1689ca111a6a5ec981f24
MarceDistefano <marcelitadis@gmail.com>
2023-05-10 19:10:17 -0300

    agregado estadisitica para kaggle

--- src/colaborativos_catastrofe/692_ZZ_final_semillas_kaggle.r
+++ src/colaborativos_catastrofe/692_ZZ_final_semillas_kaggle.r
@@ -31,8 +31,8 @@ require("lightgbm")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "ZZ6920_kaggle"
-PARAM$exp_input  <- "HT6520_kaggle"
+PARAM$experimento  <- "ZZ6920_Estadistico<kaggle"
+PARAM$exp_input  <- "HT6510Estadistico_kaggle"
 
 #Atencion, que se procesan 5 modelos y cada uno con 5 semillas, ajuste a SUS necesidades
 PARAM$modelos_rank  <- c( 1, 2 )  #Que modelos quiero, segun su posicion en el ranking
